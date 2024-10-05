8d0092aa2ddeeaab2d1ab6da504fae264f9e361b
FrizzTradezMain <adrianj7769@outlook.com>
2024-10-04 21:31:01 -0700

    Updated Propper logging in ECON

--- SlackBot/Slack_Alerts/Periodic/ECON.py
+++ SlackBot/Slack_Alerts/Periodic/ECON.py
@@ -4,16 +4,19 @@ import slack
 from datetime import datetime
 from dotenv import load_dotenv
 from SlackBot.Slack_Alerts.Periodic.Base import Base_Periodic
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Economic(Base_Periodic):
     def __init__(self, files):
         super().__init__(files)
 
     def send_alert(self):
-        # Get Today's Data Only
+        logger.debug(f" ECON | send_alert | Note: Running")
         today = datetime.now()
         today_str = today.strftime('%m/%d/%Y')
-        print(f"Fetching economic data for {today_str}...")
+        logger.debug(f" ECON | send_alert | Note: Fetching economic data for {today_str}...")
 
         # Get the economic calendar for the day
         try:
@@ -27,24 +30,19 @@ class Economic(Base_Periodic):
                 to_date=None
             )
         except Exception as e:
-            print(f"Error fetching economic calendar: {e}")
+            logger.debug(f" ECON | send_alert | Note: Error fetching economic calendar {e}")
             return
 
-        # Convert to DataFrame and manipulate data
-        print("Manipulating Dataframe...")
+        logger.debug(f" ECON | send_alert | Note: Manipulating Dataframe")
+        
         calendar_df = pd.DataFrame(calendar)
         columns_to_keep = ['time', 'event', 'importance']
         calendar_df = calendar_df[columns_to_keep]
-
-        # Store the formatted events in a list
+        
         formatted_events = [self.format_event(row) for _, row in calendar_df.iterrows()]
-
-        # Join the events into a single message with line breaks
         formatted_message = "\n".join(formatted_events) if formatted_events else "No significant events today."
 
-        print("Sending to Slack...")
-        # Send the formatted message
-        slack_channel = "econ_outlook"  # Replace with your actual Slack channel
+        slack_channel = "econ_outlook"  
         header_message = (
             f":black_large_square: *Economic Outlook for {today_str}* :black_large_square:\n"
             "───────────────────────────\n"
@@ -54,23 +52,22 @@ class Economic(Base_Periodic):
         full_message = f"{header_message}{formatted_message}{footer_message}"
 
         self.send_slack_message(slack_channel, full_message)
-        print("Completed...")
+        logger.info(f" ECON | send_alert | Channel: econ_outlook | Note: Message Sent")
 
     def format_event(self, row):
+        logger.debug(f" ECON | format_event | Note: Running")
         
-        # Extract event details
         event_time = row.get('time', 'N/A')
         event_name = row.get('event', 'N/A')
         importance = row.get('importance', 'N/A').capitalize()
 
-        # Map importance levels to emojis
         importance_emojis = {
             'High': ':red_circle:',
             'Medium': ':large_orange_diamond:',
             'Low': ':white_circle:'
         }
+        
         importance_emoji = importance_emojis.get(importance, ':grey_question:')
-
-        # Construct a formatted string for each event
         formatted_event = f">{importance_emoji}  *{event_time}* - *{event_name}*"
+        
         return formatted_event
